ed2e1df0be92fa393ef625226e613aef36f7acc9
pachauriyash <86353193+pachauriyash@users.noreply.github.com>
2023-05-28 20:46:55 +0530

    Time: 20 ms (7.57%), Space: 35.6 MB (5.48%) - LeetHub

--- 0050-powx-n/0050-powx-n.cpp
+++ 0050-powx-n/0050-powx-n.cpp
@@ -1,16 +1,18 @@
 class Solution {
 public:
+    double rec(double x,int n){
+        if(n<0){
+            return (1/x)*myPow(x,n+1);
+        }
+        if(n>0){
+             return x*myPow(x,n-1);
+        }
+        return 1.0;
+    }
     double myPow(double x, int n) {
         if(n==0){return 1.0;}
         if (n == INT_MAX) return (x == 1) ? 1 : (x == -1) ? -1 : 0;
         if (n == INT_MIN) return (x == 1 || x == -1) ? 1 : 0;
-        double ans;
-        if(n<0){
-            ans= (1/x)*myPow(x,n+1);
-        }
-        if(n>0){
-             ans= x*myPow(x,n-1);
-        }
-        return ans;
+        return rec(x,n);
     }
 };

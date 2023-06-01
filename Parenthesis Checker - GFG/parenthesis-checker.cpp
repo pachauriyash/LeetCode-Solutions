c4e0555a4a78db55fd304dca3c4f625071988a04
pachauriyash <86353193+pachauriyash@users.noreply.github.com>
2023-06-01 13:22:45 +0530

    Added solution - LeetHub

---
+++ Parenthesis Checker - GFG/parenthesis-checker.cpp
@@ -0,0 +1,69 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+
+class Solution
+{
+    public:
+    //Function to check if brackets are balanced or not.
+    bool ispar(string x)
+    {
+        stack<char> st;
+        for(int i=0;i<x.size();i++){
+            if(x[i]=='{' || x[i]=='[' || x[i]=='('){
+                st.push(x[i]);
+            }
+            else{
+                if(st.empty()){return false;}
+                else if(x[i]==']'){
+                    
+                    if(st.top()=='['){
+                        st.pop();
+                    }else{
+                        return false;
+                    }
+                }
+                else if(x[i]==')'){
+                    
+                    if(st.top()=='('){
+                        st.pop();
+                    }else{
+                        return false;
+                    }
+                }
+                else if(x[i]=='}'){
+                    if(st.top()=='{'){
+                        st.pop();
+                    }else{
+                        return false;
+                    }
+                }
+            }
+        }
+        if(!st.empty()){return false;}
+        return true;
+    }
+
+};
+
+//{ Driver Code Starts.
+
+int main()
+{
+   int t;
+   string a;
+   cin>>t;
+   while(t--)
+   {
+       cin>>a;
+       Solution obj;
+       if(obj.ispar(a))
+        cout<<"balanced"<<endl;
+       else
+        cout<<"not balanced"<<endl;
+   }
+}
+// } Driver Code Ends

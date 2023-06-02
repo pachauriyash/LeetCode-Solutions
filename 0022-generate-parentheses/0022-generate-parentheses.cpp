1c175d2ee3d1aef4ae5464f8377db92c6ff58d40
pachauriyash <86353193+pachauriyash@users.noreply.github.com>
2023-06-02 13:54:13 +0530

    Time: 4 ms (65.87%), Space: 13.6 MB (59.87%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.cpp
+++ 0022-generate-parentheses/0022-generate-parentheses.cpp
@@ -1,42 +1,23 @@
 class Solution {
 public:
-    bool isValid(string s) {
-        bool ans=false;
-        stack <char> stac;
-        for(int i=0;i<s.size();i++){
-            if(s[i]=='('){
-               stac.push(s[i]); 
-            }
-            else{
-                if(!stac.empty()){
-                    char temp=stac.top();
-                    if(s[i]==')' && temp=='('){stac.pop();}
-                    else{
-                        stac.push(s[i]);
-                    }
-                }
-                else{
-                    stac.push(s[i]);
-                }
-            }
-        }
-        if(stac.empty()){ans=true;}
-        if(!stac.empty()){ans=false;}
-        return ans;
-    }
-    void gps(vector<string> &ans,string s, int n){
-        if(n==0){
-            if(isValid(s)){ans.push_back(s);}
-            return;
-        }
-        
-        gps(ans,s+'(',n-1);
-        gps(ans,s+')',n-1);
+   void gps(vector<string> &ans,string s, int open,int close){
+       if(open==0 && close==0){
+           ans.push_back(s);
+           return;
+       }
+       if(open>0){
+           gps(ans,s+'(',open-1,close);
+       }
+       if(close>open){
+           gps(ans,s+')',open,close-1);
+       }
     }
     vector<string> generateParenthesis(int n) {
         vector<string> ans;
         string s="";
-        gps(ans,s,2*n);
+        int open=n;
+        int close=n;
+        gps(ans,s,open,close);
         return ans;
     }
 };

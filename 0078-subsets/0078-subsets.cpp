5a369df00fc2729b002746032aeb8f705114d23b
pachauriyash <86353193+pachauriyash@users.noreply.github.com>
2023-06-03 11:39:22 +0530

    Time: 7 ms (20.22%), Space: 7.1 MB (64.67%) - LeetHub

--- 0078-subsets/0078-subsets.cpp
+++ 0078-subsets/0078-subsets.cpp
@@ -1,5 +1,6 @@
 class Solution {
-public:
+public: 
+    /* recursion solution
     void recur(vector<vector<int>> &ans, vector<int> temp,int index,vector<int>& nums){
         if(index==nums.size()){
             ans.push_back(temp);
@@ -14,6 +15,30 @@ public:
         vector<vector<int>> ans;
         vector<int> temp;
         recur(ans,temp,0,nums);
+        return ans;
+    } */
+    void recur(vector<vector<int>>&ans,vector<int> nums){
+        int n = nums.size();
+	
+	for (int num = 0; num < (1 << n); num++) {
+		vector<int>temp;
+		for (int i = 0; i < n; i++) {
+			//check if the ith bit is set or not
+			if (num & (1 << i)) {
+				temp.push_back(nums[i]);
+			}
+		}
+		
+			ans.push_back(temp);
+		
+	}
+	sort(ans.begin(), ans.end());
+    }
+    vector<vector<int>> subsets(vector<int>& nums) {
+        
+        vector<vector<int>>ans;
+        recur(ans,nums);
+        
         return ans;
     }
 };

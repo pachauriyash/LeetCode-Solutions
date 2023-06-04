2b3c762dcf2939085ae6bf579ec3f867d64774b0
pachauriyash <86353193+pachauriyash@users.noreply.github.com>
2023-06-04 17:09:03 +0530

    Time: 3 ms (69.23%), Space: 7.7 MB (51.70%) - LeetHub

--- 0046-permutations/0046-permutations.cpp
+++ 0046-permutations/0046-permutations.cpp
@@ -1,5 +1,6 @@
 class Solution {
 public:
+    /* Recursive method
     void recur(vector<vector<int>>&ans,vector<int>nums,vector<int>temp){
         if(nums.size()==0){
             ans.push_back(temp);
@@ -19,5 +20,23 @@ public:
         vector<int> temp;
         recur(ans,nums,temp);
         return ans;
+    } */
+    //space optimised solution without any extra space O(1) space
+    void recur(vector<vector<int>>&ans,vector<int>&nums,int index){
+        if(index==nums.size()){
+            ans.push_back(nums);
+            return;
+        }
+        for(int i=index;i<nums.size();i++){
+            swap(nums[index],nums[i]);
+            recur(ans,nums,index+1);
+            swap(nums[index],nums[i]);
+        }
     }
+    vector<vector<int>> permute(vector<int>& nums) {
+        vector<vector<int>> ans;
+        recur(ans,nums,0);
+        return ans;
+    }
+    
 };

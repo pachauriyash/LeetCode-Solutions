35b1d5a565fae73c2b1f6512f16773bd630d8566
pachauriyash <86353193+pachauriyash@users.noreply.github.com>
2023-06-22 01:15:56 +0530

    Time: 22 ms (58.01%), Space: 19.5 MB (95.82%) - LeetHub

--- 0169-majority-element/0169-majority-element.cpp
+++ 0169-majority-element/0169-majority-element.cpp
@@ -1,7 +1,8 @@
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
-       sort(nums.begin(),nums.end());
+      /*
+        sort(nums.begin(),nums.end());
        int count=1;
        for(int i=1;i<nums.size();i++){
            if(nums[i]==nums[i-1]){count++;}
@@ -15,6 +16,28 @@ public:
                    return nums[i];
                }
        } 
-       return nums[0];
+       return nums[0]; */
+        //better approach can be using hashmap
+        //optimised approach would be using moore's voting
+        int element;
+        int count=0;
+        for(int i=0;i<nums.size();i++){
+            if(count==0){
+                element=nums[i];
+                count++;
+            }
+            else if(nums[i]==element){
+                count++;
+            }
+            else{
+                count--;
+            }
+        }
+        int count2=0;
+        for(int i=0;i<nums.size();i++){
+            if(nums[i]==element){count2++;}
+        }
+        if(count2>nums.size()/2){return element;}
+        return -1;
     }
 };

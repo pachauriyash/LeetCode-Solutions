2a5f825031c431cbf065088c30630129d0edbc26
pachauriyash <86353193+pachauriyash@users.noreply.github.com>
2023-06-22 13:13:35 +0530

    Time: 3 ms (38.24%), Space: 6 MB (86.40%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -10,8 +10,19 @@ public:
         return dphash[row][col]= recur(m,n,row+1,col,dphash) +recur(m,n,row,col+1,dphash);
     }
     int uniquePaths(int m, int n) {
-
+/*  this was optimising recursion to use dp
         vector<vector<int>> dphash(m,vector<int>(n,-1));
         return recur(m,n,0,0,dphash);
+        */
+        //now the crazy apprach it is using maths and permutation and combination
+        //so we know to reach the bottom we have to go m-1 times down and n-1 times right and total steps to go will be m+n-2 now take example of 2X3 so you have 3 steps to take out of which 1 should be down and 2 should be right so you have to fill three boxes in such a manner
+        int down=m-1;
+        int right=n-1;
+        int total=down+right;
+        double result=1;
+        for(int i=1;i<=down;i++){
+            result=result*(total-down+i)/i;
+        }
+        return (int)result;
     }
 };

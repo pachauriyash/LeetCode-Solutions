8a8d99a7c2849bfe3daf1ce706d9a5b2c73d4ff0
pachauriyash <86353193+pachauriyash@users.noreply.github.com>
2023-07-04 01:30:33 +0530

    Time: 0 ms (100.00%), Space: 7.8 MB (17.34%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -1,15 +1,26 @@
 class Solution {
 public:
+    int recur(int index,vector<int> &nums,vector<int>&dp){
+    if(index==0)return nums[index];
+    if(index<0)return 0;
+    if(dp[index]!=-1)return dp[index];
+    int pick=nums[index]+recur(index-2,nums,dp);
+    int notpick=recur(index-1,nums,dp);
+    return dp[index]=max(pick,notpick);
+}
     int rob(vector<int>& nums) {
-        int curr;
-    int prev2=nums[0];
-    if(nums.size()==1){return prev2;}
-    int prev=max(nums[1],prev2);
-    for(int i=2;i<nums.size();i++){
-        curr=max(nums[i]+prev2,prev);
-        prev2=prev;
-        prev=curr;
-    }
-    return prev;
+        vector<int> dp(nums.size(),-1);
+        return recur(nums.size()-1,nums,dp);
+        //this solution is same as Maximum sum of non-adjacent elements in striver dp sheet and here used direct space optimised solution but can be done with memorization first
+    //     int curr;
+    // int prev2=nums[0];
+    // if(nums.size()==1){return prev2;}
+    // int prev=max(nums[1],prev2);
+    // for(int i=2;i<nums.size();i++){
+    //     curr=max(nums[i]+prev2,prev);
+    //     prev2=prev;
+    //     prev=curr;
+    // }
+    // return prev;
     }
 };

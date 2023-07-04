35d51c744a43fe73dabf5bd31d37dd16030ee9c6
pachauriyash <86353193+pachauriyash@users.noreply.github.com>
2023-07-04 14:07:47 +0530

    Time: 18 ms (7.62%), Space: 10.1 MB (44.77%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.cpp
+++ 0064-minimum-path-sum/0064-minimum-path-sum.cpp
@@ -11,9 +11,28 @@ public:
         return dp[row][col]=min(left,up);
     }
     int minPathSum(vector<vector<int>>& grid) {
+        //memoriztion solution TC O(N*M) SC O(2*N*M)
         int n=grid.size();
         int m=grid[0].size();
         vector<vector<int>> dp(n,vector<int>(m,-1));
-        return recur(n-1,m-1,grid,dp);
+        // return recur(n-1,m-1,grid,dp);
+        //tabulation solution
+        
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(i==0 && j==0)dp[0][0]=grid[0][0];
+                else{
+                    int up=grid[i][j];
+                    int left=grid[i][j];
+                    if(i>0)up+=dp[i-1][j];
+                    else{up=INT_MAX;}
+                    if(j>0)left+=dp[i][j-1];
+                    else{left=INT_MAX;}
+                    dp[i][j]=min(up,left);
+                }
+            }
+        }
+        return dp[n-1][m-1];
+        
     }
 };

e34b9a4aef5855f6b3ae9172e98c6e2230cb1ad9
pachauriyash <86353193+pachauriyash@users.noreply.github.com>
2023-07-04 22:57:48 +0530

    Time: 20 ms (39.44%), Space: 10.7 MB (28.23%) - LeetHub

--- 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.cpp
+++ 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.cpp
@@ -12,10 +12,31 @@ public:
         return dp[row][col]=min(down,min(leftdiag,rightdiag));
     }
     int minFallingPathSum(vector<vector<int>>& matrix) {
-        int ans=INT_MAX;
-        for(int i=0;i<matrix.size();i++){
-            vector<vector<int>> dp(matrix.size(),vector<int>(matrix.size(),INT_MAX));
-            ans=min(ans,recur(0,i,matrix,dp));
+        //memorization TC O(N*N) SC O(N*N)+O(N)
+        // int ans=INT_MAX;
+        // for(int i=0;i<matrix.size();i++){
+        //     vector<vector<int>> dp(matrix.size(),vector<int>(matrix.size(),INT_MAX));
+        //     ans=min(ans,recur(0,i,matrix,dp));
+        // }
+        // return ans;
+        
+        //now tabulation but my memorization was from 0,0 to bottom so if from that i would have to make tabulation then base cases will be be bottom row but here i'm going from up to down as in striver video
+        int n=matrix.size();
+        vector<vector<int>> dp(n,vector<int>(n,INT_MAX));
+        for(int j=0;j<n;j++)dp[0][j]=matrix[0][j];
+        for(int i=1;i<n;i++){
+            for(int j=0;j<n;j++){
+                int down=matrix[i][j]+dp[i-1][j];
+                int leftdiag=INT_MAX;
+                int rightdiag=INT_MAX;
+                if(j>0){leftdiag=matrix[i][j]+dp[i-1][j-1];}
+                if(j<n-1){rightdiag=matrix[i][j]+dp[i-1][j+1];}
+                dp[i][j]=min(down,min(leftdiag,rightdiag));
+            }
+        }
+        int ans=dp[n-1][0];
+        for(int j=1;j<n;j++){
+            ans=min(ans,dp[n-1][j]);
         }
         return ans;
     }

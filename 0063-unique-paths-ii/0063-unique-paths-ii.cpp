c5ca8464665f65557da194131dec060604d1a36f
pachauriyash <86353193+pachauriyash@users.noreply.github.com>
2023-07-04 13:29:32 +0530

    Time: 0 ms (100.00%), Space: 7.8 MB (63.30%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.cpp
+++ 0063-unique-paths-ii/0063-unique-paths-ii.cpp
@@ -13,7 +13,23 @@ public:
         int n=obstacleGrid[0].size();
         int m=obstacleGrid.size();
         if(obstacleGrid[m-1][n-1]==1){return 0;}
-        vector<vector<int>> dphash(m,vector<int>(n,-1));
-        return recur(m,n,0,0,dphash,obstacleGrid);
+        //vector<vector<int>> dphash(m,vector<int>(n,-1));
+        // return recur(m,n,0,0,dphash,obstacleGrid);
+        vector<vector<int>> dp(m,vector<int>(n,-1));
+        dp[0][0]=0;
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(i==0 && j==0 && obstacleGrid[i][j]!=1)dp[i][j]=1;
+                else{
+                    int up=0,left=0;
+                    //because in my memoriation i used from 0,0 to m-1,n-1 so it was down and right
+                    if(i>0)up=dp[i-1][j];
+                    if(j>0)left=dp[i][j-1];
+                    dp[i][j]=left+up;
+                    if(obstacleGrid[i][j]==1)dp[i][j]=0;
+                }
+            }
+        }
+        return dp[m-1][n-1];
     }
 };

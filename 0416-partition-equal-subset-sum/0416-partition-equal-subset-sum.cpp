ffcf9048f8d11dc12bc3cc01f00ab191d86027a5
pachauriyash <86353193+pachauriyash@users.noreply.github.com>
2023-07-05 16:18:35 +0530

    Time: 517 ms (21.30%), Space: 13 MB (73.20%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
@@ -1,5 +1,6 @@
 class Solution {
 public:
+    //memorization solution
     bool recur(int index,int target,vector<int>&arr,vector<vector<int>>&dp){
     if(target==0)return true;
     if(index==0)return arr[0]==target;
@@ -10,12 +11,30 @@ public:
     return dp[index][target]=take || nottake;
 }
     bool canPartition(vector<int>& nums) {
+    
         int n=nums.size();
         int sum=0;
         for(int i=0;i<n;i++)sum+=nums[i];
         if(sum%2!=0)return false;
         int k=sum/2;
-        vector<vector<int>>dp(n,vector<int>(k+1,-1));
-        return recur(n-1,k,nums,dp);
+        if(n==1){return false;}
+        //memorization 
+        // vector<vector<int>>dp(n,vector<int>(k+1,-1));
+        // return recur(n-1,k,nums,dp);
+        //tabulation
+        
+        vector<vector<bool>>dp(n,vector<bool>(k+1,0));
+    //base cases
+    for(int i=0;i<n;i++){dp[i][0]=true;}
+    if(nums[0]<=k)dp[0][nums[0]]=true; 
+    for(int index=1;index<n;index++){
+        for(int target=1;target<=k;target++){
+            bool nottake=dp[index-1][target];
+            bool take=false;
+            if(nums[index]<=target)take=dp[index-1][target-nums[index]];
+            dp[index][target]=take || nottake;
+        }
+    }
+    return dp[n-1][k];
     }
 };

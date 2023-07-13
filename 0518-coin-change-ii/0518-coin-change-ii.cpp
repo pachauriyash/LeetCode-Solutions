c22c295b89c9985f22662dee6a9d7f94ae65acc4
pachauriyash <86353193+pachauriyash@users.noreply.github.com>
2023-07-14 00:34:39 +0530

    Time: 54 ms (32.29%), Space: 18.3 MB (63.40%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.cpp
+++ 0518-coin-change-ii/0518-coin-change-ii.cpp
@@ -12,7 +12,23 @@ public:
         return dp[index][target]=pick+notpick;
     }
     int change(int amount, vector<int>& coins) {
-        vector<vector<int>>dp(coins.size(),vector<int>(amount+1,-1));
-       return recur(coins.size()-1,amount,coins,dp);
+        //memorization TC O(N*amount) SC O(N*amount)+O(N)
+       //  vector<vector<int>>dp(coins.size(),vector<int>(amount+1,-1));
+       // return recur(coins.size()-1,amount,coins,dp);
+        //tabulation TC O(N*amount) SC O(N*amount)
+        vector<vector<int>>dp(coins.size(),vector<int>(amount+1,0));
+        for(int target=0;target<=amount;target++){
+           if(target%coins[0]==0)dp[0][target]=1;
+            else dp[0][target]=0;
+        }
+        for(int index=1;index<coins.size();index++){
+            for(int target=0;target<=amount;target++){
+                int notpick=0+dp[index-1][target];
+                int pick=0;
+                if(coins[index]<=target)pick=dp[index][target-coins[index]];
+                dp[index][target]=pick+notpick;
+            }
+        }
+         return dp[coins.size()-1][amount];
     }
 };

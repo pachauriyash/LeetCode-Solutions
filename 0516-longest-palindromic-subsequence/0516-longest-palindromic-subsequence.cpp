bd76fe6bf1a5ba516b475850818e65c4dcc8a46d
pachauriyash <86353193+pachauriyash@users.noreply.github.com>
2023-07-14 23:51:25 +0530

    Time: 242 ms (28.25%), Space: 73 MB (55.27%) - LeetHub

--- 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.cpp
+++ 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.cpp
@@ -11,7 +11,16 @@ public:
         for(int i=text1.length()-1;i>=0;i--){
             text2+=text1[i];
         }
-         vector<vector<int>>dp(text1.length(),vector<int>(text2.length(),-1));
-        return recur(text1.length()-1,text2.length()-1,text1,text2,dp);
+        //  vector<vector<int>>dp(text1.length(),vector<int>(text2.length(),-1));
+        // return recur(text1.length()-1,text2.length()-1,text1,text2,dp);
+        //tabulation TC O(N*M) SC O(N*M)
+        vector<vector<int>>dp(text1.length()+1,vector<int>(text2.length()+1,0));
+        for(int i=1;i<=text1.length();i++){
+            for(int j=1;j<=text2.length();j++){
+                if(text1[i-1]==text2[j-1]){dp[i][j]=1+dp[i-1][j-1];}
+                else{dp[i][j]=max(dp[i-1][j],dp[i][j-1]);}
+            }
+        }
+        return dp[text1.length()][text2.length()];
     }
 };

4d6e553fca2e5b7289966ea63fa3a515ac398e62
pachauriyash <86353193+pachauriyash@users.noreply.github.com>
2023-07-15 00:01:45 +0530

    Time: 149 ms (6.35%), Space: 28.5 MB (43.31%) - LeetHub

---
+++ 1312-minimum-insertion-steps-to-make-a-string-palindrome/1312-minimum-insertion-steps-to-make-a-string-palindrome.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int recur(int index1,int index2,string &text1, string &text2,vector<vector<int>>&dp){
+        if(index1<0 || index2<0){return 0;}
+        if(dp[index1][index2]!=-1)return dp[index1][index2];
+        if(text1[index1]==text2[index2]){return 1+recur(index1-1,index2-1,text1,text2,dp);}
+        return dp[index1][index2]=max(recur(index1-1,index2,text1,text2,dp),recur(index1,index2-1,text1,text2,dp));
+    }
+    
+    int minInsertions(string text1) {
+        //we'll use longest palindrominc subsequence code here and the logic is expalined in notes and video
+        string text2="";
+        for(int i=text1.length()-1;i>=0;i--){
+            text2+=text1[i];
+        }
+         vector<vector<int>>dp(text1.length(),vector<int>(text2.length(),-1));
+        return text1.length()-recur(text1.length()-1,text2.length()-1,text1,text2,dp);
+    }
+};

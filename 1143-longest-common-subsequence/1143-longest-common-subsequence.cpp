ffcf61bc1a69052fb8b1ebf2f58b93ba4c5b00e9
pachauriyash <86353193+pachauriyash@users.noreply.github.com>
2023-07-14 16:08:18 +0530

    Time: 51 ms (79.58%), Space: 6.4 MB (99.20%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
@@ -11,14 +11,24 @@ public:
         // vector<vector<int>>dp(text1.length(),vector<int>(text2.length(),-1));
         // return recur(text1.length()-1,text2.length()-1,text1,text2,dp);
         //tabulation TC O(N*M) SC O(N*M)
-        vector<vector<int>>dp(text1.length()+1,vector<int>(text2.length()+1,0));
-        //if(text1[0]==text2[0]){dp[0][0]=1;}
+        // vector<vector<int>>dp(text1.length()+1,vector<int>(text2.length()+1,0));
+        // //if(text1[0]==text2[0]){dp[0][0]=1;}
+        // for(int i=1;i<=text1.length();i++){
+        //     for(int j=1;j<=text2.length();j++){
+        //         if(text1[i-1]==text2[j-1]){dp[i][j]=1+dp[i-1][j-1];}
+        //         else{dp[i][j]=max(dp[i-1][j],dp[i][j-1]);}
+        //     }
+        // }
+        // return dp[text1.length()][text2.length()];
+        //space optimised
+        vector<int>prev(text2.length()+1,0),curr(text2.length()+1,0);
         for(int i=1;i<=text1.length();i++){
             for(int j=1;j<=text2.length();j++){
-                if(text1[i-1]==text2[j-1]){dp[i][j]=1+dp[i-1][j-1];}
-                else{dp[i][j]=max(dp[i-1][j],dp[i][j-1]);}
+                if(text1[i-1]==text2[j-1]){curr[j]=1+prev[j-1];}
+                else{curr[j]=max(prev[j],curr[j-1]);}
             }
+            prev=curr;
         }
-        return dp[text1.length()][text2.length()];
+        return prev[text2.length()];
     }
 };

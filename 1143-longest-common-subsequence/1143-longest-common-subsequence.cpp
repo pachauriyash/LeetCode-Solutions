223f908ce1dea6684d8c4827b418fade8e42051e
pachauriyash <86353193+pachauriyash@users.noreply.github.com>
2023-07-14 16:00:26 +0530

    Time: 100 ms (24.41%), Space: 24.5 MB (24.92%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
@@ -7,16 +7,18 @@ public:
         return dp[index1][index2]=max(recur(index1-1,index2,text1,text2,dp),recur(index1,index2-1,text1,text2,dp));
     }
     int longestCommonSubsequence(string text1, string text2) {
-        vector<vector<int>>dp(text1.length(),vector<int>(text2.length(),-1));
-        return recur(text1.length()-1,text2.length()-1,text1,text2,dp);
-        // vector<vector<int>>dp(text1.length(),vector<int>(text2.length(),0));
-        // if(text1[0]==text2[0]){dp[0][0]=1;}
-        // for(int i=1;i<text1.length();i++){
-        //     for(int j=1;j<text2.length();j++){
-        //         if(text1[i]==text2[j]){dp[i][j]=1+dp[i-1][j-1]; continue;}
-        //         dp[i][j]=max(dp[i-1][j],dp[i][j-1]);
-        //     }
-        // }
-        // return dp[text1.length()-1][text2.length()-1];
+        //memoriztion TC O(N*M) SC O(N*M) + O(N+M)
+        // vector<vector<int>>dp(text1.length(),vector<int>(text2.length(),-1));
+        // return recur(text1.length()-1,text2.length()-1,text1,text2,dp);
+        //tabulation TC O(N*M) SC O(N*M)
+        vector<vector<int>>dp(text1.length()+1,vector<int>(text2.length()+1,0));
+        //if(text1[0]==text2[0]){dp[0][0]=1;}
+        for(int i=1;i<=text1.length();i++){
+            for(int j=1;j<=text2.length();j++){
+                if(text1[i-1]==text2[j-1]){dp[i][j]=1+dp[i-1][j-1];}
+                else{dp[i][j]=max(dp[i-1][j],dp[i][j-1]);}
+            }
+        }
+        return dp[text1.length()][text2.length()];
     }
 };

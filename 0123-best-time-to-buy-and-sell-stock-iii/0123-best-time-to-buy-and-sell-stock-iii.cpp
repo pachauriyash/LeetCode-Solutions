c691f759f401d53b3a252595f4bfeb4547c0c286
pachauriyash <86353193+pachauriyash@users.noreply.github.com>
2023-07-19 15:59:25 +0530

    Time: 887 ms (11.47%), Space: 217.9 MB (15.98%) - LeetHub

--- 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     int recur(int index,int buy,int numsell,vector<int>&prices,vector<vector<vector<int>>>&dp){
         //base case
-        if(numsell==2){return 0;}
+        if(numsell==0){return 0;}
         if(index==prices.size()-1){
             if(!buy){
                 return prices[index];
@@ -17,12 +17,13 @@ public:
             profit=max(-prices[index]+recur(index+1,0,numsell,prices,dp),recur(index+1,1,numsell,prices,dp));
         }
         else{
-            profit=max(prices[index]+recur(index+1,1,numsell+1,prices,dp),recur(index+1,0,numsell,prices,dp));
+            profit=max(prices[index]+recur(index+1,1,numsell-1,prices,dp),recur(index+1,0,numsell,prices,dp));
         }
         return dp[index][buy][numsell]=profit;
     }
     int maxProfit(vector<int>& prices) {
+        //memoiation TC O(N*2*3) SC O(N*2*3)+O(N)
 vector<vector<vector<int>>> dp(prices.size(), vector<vector<int>>(2, vector<int>(3, -1)));
-        return recur(0,1,0,prices,dp);
+        return recur(0,1,2,prices,dp);
     }
 };

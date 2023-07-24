6c35700dcfecbd5ac465072cd2eaad91dc018774
pachauriyash <86353193+pachauriyash@users.noreply.github.com>
2023-07-24 16:55:25 +0530

    Time: 3 ms (86.60%), Space: 7.2 MB (63.42%) - LeetHub

--- 0032-longest-valid-parentheses/0032-longest-valid-parentheses.cpp
+++ 0032-longest-valid-parentheses/0032-longest-valid-parentheses.cpp
@@ -1,19 +1,36 @@
 class Solution {
 public:
     int longestValidParentheses(string s) {
+        // O(N^2)
+        // int maxi=0;
+        // for(int i=0;i<s.length();i++){
+        //     int open=0;
+        //     for(int j=i;j<s.length();j++){
+        //         if(s[j]=='(')open++;
+        //         else{
+        //             if(open>0){
+        //                 open--;
+        //             }
+        //             else{break;}
+        //             if(open==0){
+        //                 maxi=max(maxi,j-i+1);
+        //             }
+        //         }
+        //     }
+        // }
+        // return maxi;
+        stack<int> st;
+        st.push(-1);
         int maxi=0;
         for(int i=0;i<s.length();i++){
-            int open=0;
-            for(int j=i;j<s.length();j++){
-                if(s[j]=='(')open++;
+            if(s[i]=='(')st.push(i);
+            else{
+                st.pop();
+                if(st.empty()){
+                    st.push(i);
+                }
                 else{
-                    if(open>0){
-                        open--;
-                    }
-                    else{break;}
-                    if(open==0){
-                        maxi=max(maxi,j-i+1);
-                    }
+                    maxi=max(maxi,i-st.top());
                 }
             }
         }

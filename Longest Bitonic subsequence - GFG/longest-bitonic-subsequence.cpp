32ed9c335e18df44fa40218e99fe05f8fc48867e
pachauriyash <86353193+pachauriyash@users.noreply.github.com>
2023-07-25 14:29:42 +0530

    Added solution - LeetHub

--- Longest Bitonic subsequence - GFG/longest-bitonic-subsequence.cpp
+++ Longest Bitonic subsequence - GFG/longest-bitonic-subsequence.cpp
@@ -7,6 +7,7 @@ class Solution{
 	public:
 	int LongestBitonicSequence(vector<int>nums)
 	{
+	    int maxi=0;
         vector<int>dp(nums.size(),1);
         vector<int>dp2(nums.size(),1);
         for(int i=0;i<nums.size();i++){
@@ -16,22 +17,32 @@ class Solution{
                     dp[i]=1+dp[prev];
                 }
             }
+            if(dp[i]>maxi){
+                maxi=dp[i];
+            }
         }
-        for(int i=nums.size()-1;i>=0;i--){
+        reverse(nums.begin(),nums.end());
+        int maxi2=0;
+        for(int i=0;i<nums.size();i++){
 
-            for(int prev=nums.size()-1;prev>i;prev--){
+            for(int prev=0;prev<i;prev++){
                 if(nums[prev]<nums[i] && 1+dp2[prev]>dp2[i]){
                     dp2[i]=1+dp2[prev];
                 }
             }
+            if(dp2[i]>maxi2){
+                maxi2=dp2[i];
+            }
         }
         int ans=0;
+        reverse(dp2.begin(),dp2.end());
         for(int i=0;i<dp.size();i++){
             ans=max(ans,dp[i]+dp2[i]-1);
         }
         return ans;
 	}
 };
+
 
 //{ Driver Code Starts.
 int main(){

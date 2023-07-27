88544545e7c1c45b3a5140ec455017587400b52d
pachauriyash <86353193+pachauriyash@users.noreply.github.com>
2023-07-27 18:57:31 +0530

    Time: 23 ms (99.03%), Space: 24.8 MB (86.45%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.cpp
+++ 0189-rotate-array/0189-rotate-array.cpp
@@ -1,18 +1,34 @@
 class Solution {
 public:
     void rotate(vector<int>& nums, int k) {
-        vector<int>extra;
+        // vector<int>extra;
+        // int size=nums.size();
+        // k=k%size;
+        // for(int i=size-k;i<size;i++){
+        //     extra.push_back(nums[i]);
+        // }
+        // int index=size-k-1;
+        // for(int i=size-1;i>=k;i--,index--){
+        //     nums[i]=nums[index];
+        // }
+        // for(int i=0;i<k;i++){
+        //     nums[i]=extra[i];
+        // }
+        //TLE
+        // int size=nums.size();
+        // k=k%size;
+        // while(k>0){
+        //     int temp=nums[size-1];
+        //     for(int i=size-1;i>0;i--){
+        //         nums[i]=nums[i-1];
+        //     }
+        //     nums[0]=temp;
+        //     k--;
+        // }
         int size=nums.size();
         k=k%size;
-        for(int i=size-k;i<size;i++){
-            extra.push_back(nums[i]);
-        }
-        int index=size-k-1;
-        for(int i=size-1;i>=k;i--,index--){
-            nums[i]=nums[index];
-        }
-        for(int i=0;i<k;i++){
-            nums[i]=extra[i];
-        }
+        reverse(nums.begin(),nums.end());
+        reverse(nums.begin(),nums.begin()+k);
+        reverse(nums.begin()+k,nums.end());
     }
 };

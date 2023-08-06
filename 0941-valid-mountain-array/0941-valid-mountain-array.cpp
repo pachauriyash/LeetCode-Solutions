bd1e0dfe44f23f979b2acb1b303e103a2fb24d44
pachauriyash <86353193+pachauriyash@users.noreply.github.com>
2023-08-06 12:25:58 +0530

    Time: 15 ms (98.83%), Space: 22.4 MB (66.43%) - LeetHub

--- 0941-valid-mountain-array/0941-valid-mountain-array.cpp
+++ 0941-valid-mountain-array/0941-valid-mountain-array.cpp
@@ -2,14 +2,14 @@ class Solution {
 public:
     bool validMountainArray(vector<int>& arr) {
         int i=0;
-        if(arr.size()<3)return false;
-        for(i=0;i<arr.size()-1;i++){
+        int size=arr.size();
+        if(size<3)return false;
+        for(i=0;i<size-1;i++){
             if(arr[i]>=arr[i+1]){break;}
         }
-        cout<<i;
-        if(i==0 || i==arr.size()-1)return false;
+        if(i==0 || i==size-1)return false;
         
-        for(int j=i;j<arr.size()-1;j++){
+        for(int j=i;j<size-1;j++){
             if(arr[j]<=arr[j+1]){return false;}
         }
         return true;

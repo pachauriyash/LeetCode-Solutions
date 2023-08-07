35b048f51372e620ff5c2c0d1c9c0d7706bf1946
pachauriyash <86353193+pachauriyash@users.noreply.github.com>
2023-08-07 19:01:03 +0530

    Time: 756 ms (38.10%), Space: 116 MB (49.59%) - LeetHub

--- 1461-check-if-a-string-contains-all-binary-codes-of-size-k/1461-check-if-a-string-contains-all-binary-codes-of-size-k.cpp
+++ 1461-check-if-a-string-contains-all-binary-codes-of-size-k/1461-check-if-a-string-contains-all-binary-codes-of-size-k.cpp
@@ -5,12 +5,14 @@ public:
         if (s.length()<k){
             return false;
         }
+        int temp=pow(2,k);
         for(int i=0;i<=s.length()-k;i++){
             cout<<i;
             st.insert(s.substr(i,k));
+            if(st.size()==temp)return true;
         }
-        int temp=pow(2,k);
-        if(st.size()==temp)return true;
+        
+        
         return false;
         
     }

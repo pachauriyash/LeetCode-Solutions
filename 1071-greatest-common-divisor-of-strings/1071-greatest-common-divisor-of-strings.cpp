6590ebacdf13e0cdf004cb5ad5a16efb729d3cc5
pachauriyash <86353193+pachauriyash@users.noreply.github.com>
2023-08-08 10:27:09 +0530

    Time: 0 ms (100.00%), Space: 7.1 MB (55.49%) - LeetHub

--- 1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.cpp
+++ 1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.cpp
@@ -2,18 +2,22 @@ class Solution {
 public:
     string gcdOfStrings(string str1, string str2) {
         int gcds=gcd(str1.length(),str2.length());
-        if(str1.substr(0,gcds)!=str2.substr(0,gcds)){return "";}
-        string temp=str1.substr(0,gcds);
-        string temp2="";
-        for(int i=0;i<str1.length()/gcds;i++){
-            temp2+=temp;
+        // if(str1.substr(0,gcds)!=str2.substr(0,gcds)){return "";}
+        // string temp=str1.substr(0,gcds);
+        // string temp2="";
+        // for(int i=0;i<str1.length()/gcds;i++){
+        //     temp2+=temp;
+        // }
+        // if(temp2!=str1)return "";
+        // temp2="";
+        // for(int i=0;i<str2.length()/gcds;i++){
+        //     temp2+=temp;
+        // }
+        // if(temp2!=str2)return "";
+        // return temp;
+        if(str1+str2==str2+str1){
+            return str1.substr(0,gcds);
         }
-        if(temp2!=str1)return "";
-        temp2="";
-        for(int i=0;i<str2.length()/gcds;i++){
-            temp2+=temp;
-        }
-        if(temp2!=str2)return "";
-        return temp;
+        return "";
     }
 };

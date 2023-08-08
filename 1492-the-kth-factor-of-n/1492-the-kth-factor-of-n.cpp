b88c58d57a9cac610c613b5614f77c2ac486945a
pachauriyash <86353193+pachauriyash@users.noreply.github.com>
2023-08-09 00:42:45 +0530

    Time: 3 ms (36.66%), Space: 6.1 MB (9.03%) - LeetHub

--- 1492-the-kth-factor-of-n/1492-the-kth-factor-of-n.cpp
+++ 1492-the-kth-factor-of-n/1492-the-kth-factor-of-n.cpp
@@ -3,8 +3,12 @@ public:
     int kthFactor(int n, int k) {
         vector<int>tt;
         int temp=1;
+        int size=0;
         while(temp<=n/2){
-            if(n%temp==0){tt.push_back(temp);}
+            if(n%temp==0){tt.push_back(temp);
+                             size++;
+                          if(size==k)return tt[k-1];
+            }
             temp++;
         }
         tt.push_back(n);

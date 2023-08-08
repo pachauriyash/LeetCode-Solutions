b25a2a2910acef8b0ec43383ad93eda97c9d5661
pachauriyash <86353193+pachauriyash@users.noreply.github.com>
2023-08-08 16:58:37 +0530

    Time: 10 ms (96.32%), Space: 14.4 MB (21.28%) - LeetHub

--- 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.cpp
+++ 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.cpp
@@ -22,16 +22,24 @@ public:
         }
         pre.pop_back();
     }
+    
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
-        vector<TreeNode*>pre1,pre2;
-        preorder(root,p,pre1);
-        preorder(root,q,pre2);
-        TreeNode* prev=NULL;
-        int tt=min(pre1.size(),pre2.size());
-        for(int i=0;i<tt;i++){
-            if(pre1[i]!=pre2[i])return prev;
-            prev=pre1[i];
-        }
-        return prev;
+        // vector<TreeNode*>pre1,pre2;
+        // preorder(root,p,pre1);
+        // preorder(root,q,pre2);
+        // TreeNode* prev=NULL;
+        // int tt=min(pre1.size(),pre2.size());
+        // for(int i=0;i<tt;i++){
+        //     if(pre1[i]!=pre2[i])return prev;
+        //     prev=pre1[i];
+        // }
+        // return prev;
+        if(root==NULL)return NULL;
+        if(root==p || root==q){return root;}
+        TreeNode* left=lowestCommonAncestor(root->left,p,q);
+        TreeNode* right=lowestCommonAncestor(root->right,p,q);
+        if((left==p && right==q)||(left==q && right==p)){return root;}
+        if(left==NULL && right!=NULL){return right;}
+        return left;
     }
 };

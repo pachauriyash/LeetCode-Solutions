d6924f850ef148f7e8bdfa7fcc81c5c42e60125d
pachauriyash <86353193+pachauriyash@users.noreply.github.com>
2023-08-09 08:26:20 +0530

    Time: 0 ms (100.00%), Space: 6 MB (24.49%) - LeetHub

--- 2544-alternating-digit-sum/2544-alternating-digit-sum.cpp
+++ 2544-alternating-digit-sum/2544-alternating-digit-sum.cpp
@@ -2,18 +2,26 @@ class Solution {
 public:
     int alternateDigitSum(int n) {
         vector<int>digi;
+        int ans=0;
+        int i=0;
         while(n>0){
-            digi.insert(digi.begin(),n%10);
+            if(i%2==0){
+                ans+=n%10;
+            }else{
+                ans-=n%10;
+            }
+            i++;
+            //digi.insert(digi.begin(),n%10);
             n/=10;
         }
-        int ans=0;
-        for(int i=0;i<digi.size();i++){
-            if(i%2==0){
-                ans+=digi[i];
-            }else{
-                ans-=digi[i];
-            }
-        }
+        if(i%2==0)return -ans;
+        // for(int i=0;i<digi.size();i++){
+        //     if(i%2==0){
+        //         ans+=digi[i];
+        //     }else{
+        //         ans-=digi[i];
+        //     }
+        // }
         return ans;
     }
 };

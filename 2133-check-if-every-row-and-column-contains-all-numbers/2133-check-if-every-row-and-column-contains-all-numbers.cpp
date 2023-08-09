f620d175693420536b7b0f217e36d710ad845a62
pachauriyash <86353193+pachauriyash@users.noreply.github.com>
2023-08-09 19:48:46 +0530

    Time: 219 ms (71.97%), Space: 91.9 MB (43.70%) - LeetHub

--- 2133-check-if-every-row-and-column-contains-all-numbers/2133-check-if-every-row-and-column-contains-all-numbers.cpp
+++ 2133-check-if-every-row-and-column-contains-all-numbers/2133-check-if-every-row-and-column-contains-all-numbers.cpp
@@ -1,20 +1,36 @@
 class Solution {
 public:
     bool checkValid(vector<vector<int>>& matrix) {
-        unordered_map<int,unordered_set<int>>mprow,mpcol;
-        for(int i=0;i<matrix.size();i++){
-            for(int j=0;j<matrix[0].size();j++){
-                mpcol[j].insert(matrix[i][j]);
-                mprow[i].insert(matrix[i][j]);
+        // unordered_map<int,unordered_set<int>>mprow,mpcol;
+        // for(int i=0;i<matrix.size();i++){
+        //     for(int j=0;j<matrix[0].size();j++){
+        //         mpcol[j].insert(matrix[i][j]);
+        //         mprow[i].insert(matrix[i][j]);
+        //     }
+        // }
+        // if(mprow.size()!=matrix.size())return false;
+        // if(mpcol.size()!=matrix.size())return false;
+        // for(auto it:mprow){
+        //     if(it.second.size()!=matrix[0].size())return false;
+        // }
+        // for(auto it:mpcol){
+        //     if(it.second.size()!=matrix.size())return false;
+        // }
+        // return true;
+        int n=matrix.size();
+        for(int i=0;i<n;i++){
+            unordered_set<int>mp;
+            for(int j=0;j<n;j++){
+                mp.insert(matrix[i][j]);
             }
+            if(mp.size()!=n)return false;
         }
-        if(mprow.size()!=matrix.size())return false;
-        if(mpcol.size()!=matrix.size())return false;
-        for(auto it:mprow){
-            if(it.second.size()!=matrix[0].size())return false;
-        }
-        for(auto it:mpcol){
-            if(it.second.size()!=matrix.size())return false;
+        for(int i=0;i<n;i++){
+            unordered_set<int>mp;
+            for(int j=0;j<n;j++){
+                mp.insert(matrix[j][i]);
+            }
+            if(mp.size()!=n)return false;
         }
         return true;
     }

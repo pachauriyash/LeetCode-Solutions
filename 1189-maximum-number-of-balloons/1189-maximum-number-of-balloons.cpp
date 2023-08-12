8ec50b7d2f8094fcc59c3d2f5139045867380940
pachauriyash <86353193+pachauriyash@users.noreply.github.com>
2023-08-12 09:38:52 +0530

    Time: 2 ms (74.07%), Space: 6.6 MB (30.15%) - LeetHub

--- 1189-maximum-number-of-balloons/1189-maximum-number-of-balloons.cpp
+++ 1189-maximum-number-of-balloons/1189-maximum-number-of-balloons.cpp
@@ -3,14 +3,13 @@ public:
     int maxNumberOfBalloons(string text) {
         map<char,int>mp;
         for(int i=0;i<text.size();i++){
-            if(mp.find(text[i])!=mp.end()){
+            if(text[i]=='b'||text[i]=='a'||text[i]=='l'||text[i]=='o'||text[i]=='n'){if(mp.find(text[i])!=mp.end()){
                 mp[text[i]]++;
             }
-            else{mp.insert({text[i],1});}
+            else{mp.insert({text[i],1});}}
         }
         int temp=0,temp2=0;
         if(mp['b']==0 || mp['a']==0 || mp['l']<2 || mp['o']<2 || mp['n']==0)return 0;
-        cout<<mp['b']<<" "<<mp['a']<<" "<<mp['l']<<" "<<mp['o']<<" "<<mp['n'];
         temp=min(mp['o'],mp['l'])/2;
         temp=min(mp['b'],min(mp['a'],min(mp['n'],temp)));
         return temp;

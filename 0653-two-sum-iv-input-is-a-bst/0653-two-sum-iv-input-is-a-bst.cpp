1ea0e9d2fc4e24235176c9595a3d67d0a45ab36a
pachauriyash <86353193+pachauriyash@users.noreply.github.com>
2023-08-17 10:44:22 +0530

    Time: 34 ms (78.18%), Space: 36.7 MB (70.48%) - LeetHub

---
+++ 0653-two-sum-iv-input-is-a-bst/0653-two-sum-iv-input-is-a-bst.cpp
@@ -0,0 +1,62 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class BSTIterator {
+public:
+    stack<TreeNode*>st;
+    bool reverse=true;
+    //reverse->true->before
+    //reverse->false->next
+    BSTIterator(TreeNode* root,bool isreverse) {
+        reverse=isreverse;
+        pushall(root);
+    }
+    
+    int next() {
+        TreeNode* temp=st.top();
+        st.pop();
+        if(!reverse)pushall(temp->right);
+        else{pushall(temp->left);}
+        return temp->val;
+       
+    }
+    //returns whether we have a next smallest or largest number
+    bool hasNext() {
+       return !st.empty();
+    }
+    void pushall(TreeNode* node){
+        while(node){
+            st.push(node);
+            if(reverse){
+                node=node->right;
+            }else{
+                node=node->left;   
+            }
+        }
+    }
+};
+class Solution {
+public:
+    
+    bool findTarget(TreeNode* root, int k) {
+        if(!root)return false;
+        BSTIterator leftt(root,false);
+        BSTIterator rightt(root,true);
+        int i=leftt.next();
+        int j=rightt.next();
+        while(i<j){
+            if(i+j==k)return true;
+            else if(i+j<k){i=leftt.next();}
+            else{j=rightt.next();}
+        }
+        return false;
+    }
+};

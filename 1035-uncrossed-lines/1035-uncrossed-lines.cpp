5944ef3da0f20278b0a6a441b533d555644a1aa2
pachauriyash <86353193+pachauriyash@users.noreply.github.com>
2023-08-22 21:51:51 +0530

    Time: 21 ms (47.22%), Space: 13 MB (44.63%) - LeetHub

--- 1035-uncrossed-lines/1035-uncrossed-lines.cpp
+++ 1035-uncrossed-lines/1035-uncrossed-lines.cpp
@@ -1,19 +1,32 @@
 class Solution {
 public:
+//     int recur(vector<int>& nums1, vector<int>& nums2,int index1,int index2,vector<vector<int>>&dp){
+//         //base case
+//         if(index1>=nums1.size() || index2>=nums2.size()){return 0;}
+//         if(dp[index1][index2]!=-1)return dp[index1][index2];
+        
+//         int index=index2;
+//         while(index<nums2.size() && nums2[index]!=nums1[index1]){index++;}
+//         int take=0;
+//         if(index!=nums2.size() && nums2[index]==nums1[index1]){
+//             //cout<<index1<<" "<<index2<<" "<<"balee";
+//             take=1+recur(nums1,nums2,index1+1,index+1,dp);
+//         }
+//         int nottake=recur(nums1,nums2,index1+1,index2,dp);
+//         return dp[index1][index2]=max(take,nottake);
+//     }
     int recur(vector<int>& nums1, vector<int>& nums2,int index1,int index2,vector<vector<int>>&dp){
         //base case
         if(index1>=nums1.size() || index2>=nums2.size()){return 0;}
         if(dp[index1][index2]!=-1)return dp[index1][index2];
-        //int index=lower_bound(nums2.begin()+index2,nums2.end(),nums1[index1])-nums2.begin(); 
-        int index=index2;
-        while(index<nums2.size() && nums2[index]!=nums1[index1]){index++;}
-        int take=0;
-        if(index!=nums2.size() && nums2[index]==nums1[index1]){
-            //cout<<index1<<" "<<index2<<" "<<"balee";
-            take=1+recur(nums1,nums2,index1+1,index+1,dp);
+        
+        
+        if(nums2[index2]!=nums1[index1]){
+            return dp[index1][index2]= max(recur(nums1,nums2,index1+1,index2,dp),recur(nums1,nums2,index1,index2+1,dp));
         }
-        int nottake=recur(nums1,nums2,index1+1,index2,dp);
-        return dp[index1][index2]=max(take,nottake);
+        else{
+             return dp[index1][index2]=1+recur(nums1,nums2,index1+1,index2+1,dp);
+        }
     }
     int maxUncrossedLines(vector<int>& nums1, vector<int>& nums2) {   
         vector<vector<int>>dp(nums1.size(),vector<int>(nums2.size(),-1));

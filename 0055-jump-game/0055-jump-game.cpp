7eea9721949b1e079e478b9cf284333df581201f
pachauriyash <86353193+pachauriyash@users.noreply.github.com>
2023-08-26 13:09:01 +0530

    Time: 49 ms (77.45%), Space: 48.4 MB (75.62%) - LeetHub

--- 0055-jump-game/0055-jump-game.cpp
+++ 0055-jump-game/0055-jump-game.cpp
@@ -1,14 +1,22 @@
 class Solution {
 public:
     bool canJump(vector<int>& nums) {
-        vector<bool>ans(nums.size(),false);
-        ans[nums.size()-1]=true;
-        for(int i=nums.size()-2;i>=0;i--){
-            for(int j=1;j<=nums[i] && i+j<nums.size();j++){
-                ans[i]=ans[i] || ans[i+j];
-                if(ans[i]==true)break;
-            }
+        //naive or kinda dp like approach but I can do it with DP as well(have done it in one cs mock interview infact a better verion of it)
+        // vector<bool>ans(nums.size(),false);
+        // ans[nums.size()-1]=true;
+        // for(int i=nums.size()-2;i>=0;i--){
+        //     for(int j=1;j<=nums[i] && i+j<nums.size();j++){
+        //         ans[i]=ans[i] || ans[i+j];
+        //         if(ans[i]==true)break;
+        //     }
+        // }
+        // return ans[0];
+        //greedy approach as solving this in greedy section(seen from comments)
+        int maxindexreached=0;
+        for(int i=0;i<nums.size();i++){
+            if(maxindexreached<i)return false;//bcz i can't even reach this current index
+            maxindexreached=max(maxindexreached,i+nums[i]);
         }
-        return ans[0];
+        return true;
     }
 };

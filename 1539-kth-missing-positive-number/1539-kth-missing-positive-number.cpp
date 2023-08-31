f6986e18d3bde4e6e0acf246e8fef6ac86febaba
pachauriyash <86353193+pachauriyash@users.noreply.github.com>
2023-08-31 20:09:06 +0530

    Time: 6 ms (37.75%), Space: 9.6 MB (88.45%) - LeetHub

--- 1539-kth-missing-positive-number/1539-kth-missing-positive-number.cpp
+++ 1539-kth-missing-positive-number/1539-kth-missing-positive-number.cpp
@@ -1,11 +1,13 @@
 class Solution {
 public:
     int findKthPositive(vector<int>& arr, int k) {
-        for (int i = 0; i < arr.size(); i++) {
-            if (arr[i] <= k) k++; //shifting k
-            else break;
-        }
-        return k;
+        //brute force TC O(N) SC O(1)
+        // for (int i = 0; i < arr.size(); i++) {
+        //     if (arr[i] <= k) k++; //shifting k
+        //     else break;
+        // }
+        // return k;
+        // brute force TC O(N) SC O(N)
         // map<int,int>mp;
         // for(int i=0;i<arr.size();i++)mp[arr[i]]++;
         // int i=1;
@@ -15,15 +17,15 @@ public:
         // }
         // return i-1;
         //binary search
-        // int low=0,high=arr.size()-1;
-        // while(low<=high){
-        //     int mid=(high+low)/2;
-        //     int missing=arr[mid]-(mid+1);
-        //     if(missing<k){low=mid+1;}
-        //     else{high=mid-1;}
-        // }
-        // if(high==-1){return k;}
-        // int missing=k-(arr[high]-(high+1));
-        // return arr[high]+missing;
+        int low=0,high=arr.size()-1;
+        while(low<=high){
+            int mid=(high+low)/2;
+            int missing=arr[mid]-(mid+1);
+            if(missing<k){low=mid+1;}
+            else{high=mid-1;}
+        }
+        if(high==-1){return k;}
+        int missing=k-(arr[high]-(high+1));
+        return arr[high]+missing;
     }
 };

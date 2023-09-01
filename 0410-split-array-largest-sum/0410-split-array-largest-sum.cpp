fed7b3c9c1217eb4e12a08c060e96721f25aaeb0
pachauriyash <86353193+pachauriyash@users.noreply.github.com>
2023-09-01 14:46:20 +0530

    Time: 4 ms (42.96%), Space: 7.3 MB (67.67%) - LeetHub

--- 0410-split-array-largest-sum/0410-split-array-largest-sum.cpp
+++ 0410-split-array-largest-sum/0410-split-array-largest-sum.cpp
@@ -17,9 +17,35 @@ public:
     }
     return dp[index][m]=ans;
 }
+    //binary search solution
+    int countstud(vector<int>& arr, int n,int limit){
+    int count=0,temp=0;  
+    for(int i=0;i<n;i++){
+        temp+=arr[i];
+        if(temp>limit){
+            count++;
+            temp=arr[i];
+        }
+    }
+    return ++count;
+}
     int splitArray(vector<int>& nums, int k) {
-        vector<vector<int>>dp(nums.size()+1,vector<int>(k+1,-1));
-        if(k>nums.size())return -1;
-        return recur(nums,0,k,dp);
+        // vector<vector<int>>dp(nums.size()+1,vector<int>(k+1,-1));
+        // if(k>nums.size())return -1;
+        // return recur(nums,0,k,dp);
+        
+        //binary search
+        int n=nums.size();
+        if(k>n)return -1;
+    int low=INT_MIN,high=INT_MAX;
+    for(int i=0;i<n;i++)low=max(low,nums[i]);
+    while(low<=high){
+        int mid=low+(high-low)/2;
+        if(countstud(nums,n,mid)>k){
+            low=mid+1;
+        }
+        else{high=mid-1;}
+    }
+    return low;
     }
 };

8680ec1ed5cc7e7ba4e3c690bb51da0031896089
pachauriyash <86353193+pachauriyash@users.noreply.github.com>
2023-09-02 01:05:04 +0530

    Time: 0 ms (100.00%), Space: 7.8 MB (65.79%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.cpp
+++ 0338-counting-bits/0338-counting-bits.cpp
@@ -9,9 +9,18 @@ public:
         return count;
     }
     vector<int> countBits(int n) {
-        vector<int>ans;
-        for(int i=0;i<=n;i++){
-            ans.push_back(numbits(i));
+        // vector<int>ans;
+        // for(int i=0;i<=n;i++){
+        //     ans.push_back(numbits(i));
+        // }
+        // return ans;
+        
+        //optimised sol
+        vector<int>ans(n+1);
+        ans[0]=0;
+        for(int i=1;i<n+1;i++){
+            //basically if x is even then it's number of set bits are same as x/2 but if odd then it is 1+ x/2
+            ans[i]=ans[i/2]+i%2;
         }
         return ans;
     }

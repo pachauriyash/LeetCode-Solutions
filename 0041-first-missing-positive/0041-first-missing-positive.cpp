7a0978d7a114f432eb9b3230eef4780966b63783
pachauriyash <86353193+pachauriyash@users.noreply.github.com>
2023-09-05 01:16:23 +0530

    Time: 45 ms (87.16%), Space: 41.1 MB (69.03%) - LeetHub

--- 0041-first-missing-positive/0041-first-missing-positive.cpp
+++ 0041-first-missing-positive/0041-first-missing-positive.cpp
@@ -1,18 +1,32 @@
 class Solution {
 public:
     int firstMissingPositive(vector<int>& nums) {
-        sort(nums.begin(),nums.end());
-        int index=0;
-        while(index<nums.size() && nums[index]<=0){
-            index++;
+        //TC O(NlogN) SC O(1)
+        // sort(nums.begin(),nums.end());
+        // int index=0;
+        // while(index<nums.size() && nums[index]<=0){
+        //     index++;
+        // }
+        // if(index==nums.size())return 1;
+        // int val=1;
+        // for(;index<nums.size();index++){
+        //     if(index!=0 && nums[index]==nums[index-1])continue;
+        //     if(nums[index]==val){val++;}
+        //     else{return val;}
+        // }
+        // return val;
+        
+        //TC O(1) SC O(1)
+        int n=nums.size();
+        for(int i=0;i<n;i++){
+            while(nums[i]>0 && nums[i]<=n && nums[nums[i]-1]!=nums[i]){
+                swap(nums[i],nums[nums[i]-1]);
+            }
         }
-        if(index==nums.size())return 1;
-        int val=1;
-        for(;index<nums.size();index++){
-            if(index!=0 && nums[index]==nums[index-1])continue;
-            if(nums[index]==val){val++;}
-            else{return val;}
+        for(int i=0;i<n;i++){
+            if(nums[i]==i+1){continue;}
+            else{return i+1;}
         }
-        return val;
+        return n+1;
     }
 };

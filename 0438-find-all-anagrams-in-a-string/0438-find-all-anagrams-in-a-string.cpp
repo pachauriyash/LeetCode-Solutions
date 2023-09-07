4c62ec5b430e4ee56fac28c0c609cf39921cba34
pachauriyash <86353193+pachauriyash@users.noreply.github.com>
2023-09-07 15:47:28 +0530

    Time: 11 ms (77.33%), Space: 8.7 MB (67.83%) - LeetHub

--- 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.cpp
+++ 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.cpp
@@ -10,27 +10,51 @@ public:
         return true;
     }
     vector<int> findAnagrams(string txt, string pat) {
+	    // int i=0,j=0;
+	    // vector<int>ans;
+	    // map<char,int>mp2,mp;
+	    // int n=pat.size();
+	    // for(int i=0;i<n;i++)mp2[pat[i]]++;
+	    // while(j<txt.size()){
+	    // mp[txt[j]]++;
+	    // if(j-i+1==n){
+	    // if(mp.size()==mp2.size() && check(mp,mp2)){
+	    // ans.push_back(i);
+	    // }
+	    // mp[txt[i]]--;
+	    // if(mp[txt[i]]==0)mp.erase(txt[i]);
+	    // i++;
+	    // j++;
+	    // }
+	    // else{
+	    // j++;
+	    // }
+	    // }
+	    // return ans;
+        
         int i=0,j=0;
-        vector<int>ans;
-	    map<char,int>mp2,mp;
-	    int n=pat.size();
-	    for(int i=0;i<n;i++)mp2[pat[i]]++;
-        while(j<txt.size()){
-            mp[txt[j]]++;
-            if(j-i+1==n){
-                if(mp.size()==mp2.size() && check(mp,mp2)){
-                    ans.push_back(i);
-                }
-                mp[txt[i]]--;
-                if(mp[txt[i]]==0)mp.erase(txt[i]);
-                i++;
-                j++;
-            }
-            else{
-                j++;
-            }
+        int n=pat.size();
+	    vector<int>ans;
+        vector<int> freq_p(26,0);
+        vector<int> window(26,0);
+	    for(int i=0;i<n;i++){
+            freq_p[pat[i]-'a']++;
         }
+        
+        while(j<txt.size()){
+	        window[txt[j]-'a']++;
+	        if(j-i+1==n){
+	            if(freq_p==window){
+	                ans.push_back(i);
+	            }
+                window[txt[i]-'a']--;
+	            i++;
+	            j++;
+	        }
+	        else{
+	            j++;
+	        }
+	    }
         return ans;
-	    
     }
 };

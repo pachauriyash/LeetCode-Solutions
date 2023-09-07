588950cce7052270f94aeaa1fa15c63e46535e33
pachauriyash <86353193+pachauriyash@users.noreply.github.com>
2023-09-07 20:42:23 +0530

    Time: 499 ms (14.75%), Space: 175.5 MB (16.44%) - LeetHub

---
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<int> maxSlidingWindow(vector<int>& nums, int k) {
+        map<int,int>mp;
+        int i=0,j=0;
+        vector<int>ans;
+        while(j<nums.size()){
+            mp[nums[j]]++;
+            if(j-i+1==k){
+                auto it=mp.rbegin();
+                ans.push_back(it->first);
+                mp[nums[i]]--;
+                if(mp[nums[i]]==0)mp.erase(nums[i]);
+                i++;
+                j++;
+            }else{j++;}
+        }
+        return ans;
+    }
+};

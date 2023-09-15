3c41f6a15db2d171fc8a705941041a61da751f44
pachauriyash <86353193+pachauriyash@users.noreply.github.com>
2023-09-16 01:09:42 +0530

    Time: 13 ms (64.37%), Space: 25.7 MB (7.53%) - LeetHub

--- 0530-minimum-absolute-difference-in-bst/0530-minimum-absolute-difference-in-bst.cpp
+++ 0530-minimum-absolute-difference-in-bst/0530-minimum-absolute-difference-in-bst.cpp
@@ -11,20 +11,35 @@
  */
 class Solution {
 public:
-    void traverse(TreeNode* root,vector<int>&preorder){
-        if(root==NULL)return;
-        traverse(root->left,preorder);
-        preorder.push_back(root->val);
-        traverse(root->right,preorder);
+    //storing preorder and then giving diff SC O(N+H)
+    // void traverse(TreeNode* root,vector<int>&preorder){
+    //     if(root==NULL)return;
+    //     traverse(root->left,preorder);
+    //     preorder.push_back(root->val);
+    //     traverse(root->right,preorder);
 
+    // }
+    // int getMinimumDifference(TreeNode* root) {
+    //     vector<int>preorder;
+    //     traverse(root,preorder);
+    //     int ans=INT_MAX;
+    //     for(int i=1;i<preorder.size();i++){
+    //         ans=min(ans,preorder[i]-preorder[i-1]);
+    //     }
+    //     return ans;
+    // }
+    // A little better way
+    int traverse(TreeNode* root,int&ans,int&prevval){
+
+        if(root->left)traverse(root->left,ans,prevval);
+        if(prevval>=0)ans=min(ans,root->val-prevval);
+        prevval=root->val;
+        if(root->right)traverse(root->right,ans,prevval);
+        return ans;
     }
     int getMinimumDifference(TreeNode* root) {
-        vector<int>preorder;
-        traverse(root,preorder);
-        int ans=INT_MAX;
-        for(int i=1;i<preorder.size();i++){
-            ans=min(ans,preorder[i]-preorder[i-1]);
-        }
+        int ans=INT_MAX,val=-1;
+        traverse(root,ans,val);
         return ans;
     }
 };

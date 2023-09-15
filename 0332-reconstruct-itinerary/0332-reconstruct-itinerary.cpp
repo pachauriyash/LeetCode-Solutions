1942b6eb5b0ca2f0ef2017a376402cd2b92ac7e1
pachauriyash <86353193+pachauriyash@users.noreply.github.com>
2023-09-15 14:34:12 +0530

    Time: 17 ms (79.74%), Space: 15.4 MB (29.55%) - LeetHub

--- 0332-reconstruct-itinerary/0332-reconstruct-itinerary.cpp
+++ 0332-reconstruct-itinerary/0332-reconstruct-itinerary.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    void dfs(map<string,vector<string>>&graph,string airport, vector<string>&ans){
+    void dfs(unordered_map<string,vector<string>>&graph,string airport, vector<string>&ans){
         while(!graph[airport].empty()){
             string next=graph[airport].back();
             graph[airport].pop_back();
@@ -9,15 +9,12 @@ public:
         ans.push_back(airport);
     }
     vector<string> findItinerary(vector<vector<string>>& tickets) {
+        //so basically we are sorting in reverse as while backtracking we are poppping from back so kind of iterating the adjacency list from back so thats why
         sort(tickets.rbegin(),tickets.rend());
-        map<string,vector<string>>graph;
+        unordered_map<string,vector<string>>graph;
         for(auto it:tickets){
             graph[it[0]].push_back(it[1]);
         }
-        // for (auto it : graph) {
-        //     sort(it.second.rbegin(),it.second.rend());
-        // }
-
         vector<string>ans;
         dfs(graph,"JFK",ans);
         reverse(ans.begin(),ans.end());

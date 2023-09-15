a058eb733a87b0f25511b2bc85a59cbef73eb42b
pachauriyash <86353193+pachauriyash@users.noreply.github.com>
2023-09-15 23:37:21 +0530

    Time: 4 ms (14.1%), Space: 7.2 MB (7.53%) - LeetHub

---
+++ 0289-game-of-life/0289-game-of-life.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    int alive(vector<vector<int>>& board,int row,int col){
+        int delrow[]={-1,-1,0,+1,+1,+1,0,-1};
+        int delcol[]={0,+1,+1,+1,0,-1,-1,-1};
+        int count=0;
+        for(int i=0;i<8;i++){
+            int nrow=delrow[i]+row;
+            int ncol=delcol[i]+col;
+            if(nrow>=0 && nrow<board.size() && ncol>=0 && ncol<board[0].size()){
+                if(board[nrow][ncol]==1)count++;
+            }
+        }
+        return count;
+    }
+    void gameOfLife(vector<vector<int>>& board) {
+        vector<vector<int>>temp(board.size(),vector<int>(board[0].size()));
+        for(int i=0;i<board.size();i++){
+            for(int j=0;j<board[0].size();j++){
+                int count=alive(board,i,j);
+                if(board[i][j]==1){
+                    if(count<2 || count>3){temp[i][j]=0;}
+                    else{temp[i][j]=1;}
+                }else{
+                    if(count==3){temp[i][j]=1;}
+                    else{temp[i][j]=0;}
+                }
+            }
+        }
+        board=temp;
+    }
+};

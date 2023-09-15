9f8d7072e398f54d18d0b3460ce2c742a3ebe01d
pachauriyash <86353193+pachauriyash@users.noreply.github.com>
2023-09-15 23:43:15 +0530

    Time: 0 ms (100%), Space: 7.2 MB (5.89%) - LeetHub

--- 0289-game-of-life/0289-game-of-life.cpp
+++ 0289-game-of-life/0289-game-of-life.cpp
@@ -8,25 +8,37 @@ public:
             int nrow=delrow[i]+row;
             int ncol=delcol[i]+col;
             if(nrow>=0 && nrow<board.size() && ncol>=0 && ncol<board[0].size()){
-                if(board[nrow][ncol]==1)count++;
+                if(board[nrow][ncol]==1 || board[nrow][ncol]==2)count++;
             }
         }
         return count;
     }
     void gameOfLife(vector<vector<int>>& board) {
-        vector<vector<int>>temp(board.size(),vector<int>(board[0].size()));
+        //vector<vector<int>>temp(board.size(),vector<int>(board[0].size()));
         for(int i=0;i<board.size();i++){
             for(int j=0;j<board[0].size();j++){
                 int count=alive(board,i,j);
                 if(board[i][j]==1){
-                    if(count<2 || count>3){temp[i][j]=0;}
-                    else{temp[i][j]=1;}
+                    if(count<2 || count>3){board[i][j]=2;}
+
                 }else{
-                    if(count==3){temp[i][j]=1;}
-                    else{temp[i][j]=0;}
+                    if(count==3){board[i][j]=3;}
+
                 }
             }
         }
-        board=temp;
+        for(int i=0;i<board.size();i++){
+            for(int j=0;j<board[0].size();j++){
+                
+                if(board[i][j]==2){
+                   board[i][j]=0;
+
+                }
+                if(board[i][j]==3){
+                    board[i][j]=1;
+                }
+            }
+        }
+
     }
 };

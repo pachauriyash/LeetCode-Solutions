b7d817bc3f17bb53e621b108c5bf7dbf82294109
pachauriyash <86353193+pachauriyash@users.noreply.github.com>
2023-09-16 13:35:52 +0530

    Time: 0 ms (100%), Space: 8.8 MB (8.12%) - LeetHub

--- 0274-h-index/0274-h-index.cpp
+++ 0274-h-index/0274-h-index.cpp
@@ -1,24 +1,34 @@
 class Solution {
 public:
     int hIndex(vector<int>& citations) {
-        sort(citations.begin(),citations.end());
-        int maxi=INT_MIN,n=citations.size();
-        int i=0,j=n;
-        // for(int k=0;k<citations.size();k++){
-        //     //i=min(i,citations[k]);
-        //     j=max(k,citations[k]);
+        
+        // sort(citations.begin(),citations.end());
+        // int maxi=INT_MIN,n=citations.size();
+        // int i=0,j=n;
+        // while(i<=j){
+        //     int mid=(i+j)/2;
+        //     int index=lower_bound(citations.begin(),citations.end(),mid)-citations.begin();
+        //     if(n-index>=mid){
+        //         maxi=max(maxi,mid);
+        //         i=mid+1;
+        //     }else{
+        //         j=mid-1;
+        //     }
         // }
-        while(i<=j){
-            int mid=(i+j)/2;
-            int index=lower_bound(citations.begin(),citations.end(),mid)-citations.begin();
-            if(n-index>=mid){
-                maxi=max(maxi,mid);
-                i=mid+1;
-            }else{
-                j=mid-1;
+       
+        // return maxi;
+        //more optimised
+        sort(citations.begin(),citations.end());
+        int n=citations.size();
+        for(int i=0;i<citations.size();i++){
+            if(citations[i]>=n)return n;
+            else{
+                if(n==1 && citations[i]!=0){
+                    return 1;
+                }
+                n--;
             }
         }
-       
-        return maxi;
+        return n;
     }
 };

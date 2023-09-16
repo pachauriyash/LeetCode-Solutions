7ceec8be727f6705de11c7e83e67e5d0e4e14fed
pachauriyash <86353193+pachauriyash@users.noreply.github.com>
2023-09-16 13:31:58 +0530

    Time: 2 ms (56.49%), Space: 8.9 MB (5.37%) - LeetHub

--- 0274-h-index/0274-h-index.cpp
+++ 0274-h-index/0274-h-index.cpp
@@ -3,11 +3,11 @@ public:
     int hIndex(vector<int>& citations) {
         sort(citations.begin(),citations.end());
         int maxi=INT_MIN,n=citations.size();
-        int i=0,j=INT_MIN;
-        for(int k=0;k<citations.size();k++){
-            //i=min(i,citations[k]);
-            j=max(k,citations[k]);
-        }
+        int i=0,j=n;
+        // for(int k=0;k<citations.size();k++){
+        //     //i=min(i,citations[k]);
+        //     j=max(k,citations[k]);
+        // }
         while(i<=j){
             int mid=(i+j)/2;
             int index=lower_bound(citations.begin(),citations.end(),mid)-citations.begin();

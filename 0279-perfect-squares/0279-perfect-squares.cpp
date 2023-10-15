069f03c848ab8ad66adcad77182231bbc6fd780a
pachauriyash <86353193+pachauriyash@users.noreply.github.com>
2023-10-15 21:26:16 +0530

    Time: 229 ms (26.33%), Space: 12.5 MB (17.17%) - LeetHub

---
+++ 0279-perfect-squares/0279-perfect-squares.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int recur(int n,vector<int>&perf,vector<int>&dp){
+        //base case
+        if(n==0)return 0;
+        if(dp[n]!=-1)return dp[n];
+        int temp=1e9;
+        for(auto it:perf){
+            if(it<=n){
+                temp=min(temp,1+recur(n-it,perf,dp));
+            }
+        }
+        return dp[n]=temp;
+    }
+    int numSquares(int n) {
+        vector<int>perf;
+        vector<int>dp(n+1,-1);
+        for(int i=1;i<=n;i++){
+            int t=sqrt(i);
+            if(t*t==i){perf.push_back(i);}
+        }
+        return recur(n,perf,dp);
+    }
+};

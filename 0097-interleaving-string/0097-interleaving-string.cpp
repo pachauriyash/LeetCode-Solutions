d01da5086ae62c535c0ad635e54b44cd96079ca4
pachauriyash <86353193+pachauriyash@users.noreply.github.com>
2023-10-15 15:24:16 +0530

    Time: 11 ms (33.88%), Space: 26 MB (22.11%) - LeetHub

--- 0097-interleaving-string/0097-interleaving-string.cpp
+++ 0097-interleaving-string/0097-interleaving-string.cpp
@@ -3,9 +3,9 @@ public:
     bool recur(int index1,int index2,int index3,string s1,string s2,string s3,vector<vector<int>>&dp){
         //base
         if(index1==s1.size() && index2==s2.size() && index3==s3.size())return true;
-        if(index1>s1.size()||index2>s2.size()){
-            return false;
-        }
+        // if(index1>s1.size()||index2>s2.size()){
+        //     return false;
+        // }
         if(dp[index1][index2]!=-1)return dp[index1][index2];
         if(s3[index3]==s1[index1] && s3[index3]==s2[index2]){
             return dp[index1][index2]= recur(index1+1,index2,index3+1,s1,s2,s3,dp) || recur(index1,index2+1,index3+1,s1,s2,s3,dp);

9535756e9c42cd8cb3fb111e660cefb0b3bf3e39
pachauriyash <86353193+pachauriyash@users.noreply.github.com>
2023-10-26 20:56:02 +0530

    Time: 265 ms (32.03%), Space: 89.2 MB (34.98%) - LeetHub

--- 2334-number-of-flowers-in-full-bloom/2334-number-of-flowers-in-full-bloom.cpp
+++ 2334-number-of-flowers-in-full-bloom/2334-number-of-flowers-in-full-bloom.cpp
@@ -1,19 +1,27 @@
 class Solution {
 public:
-    vector<int> fullBloomFlowers(vector<vector<int>>& flowers, vector<int>& persons) {
-       
-        vector<int> start, end;
-        for (auto& t : flowers)
-            start.push_back(t[0]), end.push_back(t[1]);
-        sort(start.begin(), start.end());
-        sort(end.begin(), end.end());
-        vector<int> res;
-        for (int t : persons) {
-            int started = upper_bound(start.begin(), start.end(), t) - start.begin();
-            int ended = lower_bound(end.begin(), end.end(), t) - end.begin();
-            res.push_back(started - ended);
+    vector<int> fullBloomFlowers(vector<vector<int>>& flowers, vector<int>& people) {
+        vector<int>ans;
+        //sort(flowers.begin(),flowers.end());
+        // for(int i=0;i<people.size();i++){
+        //     int count=0;
+        //     for(int j=0;j<flowers.size();j++){
+        //         if(people[i]>=flowers[j][0] && people[i]<=flowers[j][1])count++;
+        //     }
+        //     ans.push_back(count);
+        // }
+        vector<int>start,end;
+        for(auto it:flowers){
+            start.push_back(it[0]);
+            end.push_back(it[1]);
         }
-        return res;
-    
+        sort(start.begin(),start.end());
+        sort(end.begin(),end.end());
+        for(auto it:people){
+            int started=upper_bound(start.begin(),start.end(),it)-start.begin();
+            int ended=lower_bound(end.begin(),end.end(),it)-end.begin();
+            ans.push_back(started-ended);
+        }
+        return ans;
     }
 };

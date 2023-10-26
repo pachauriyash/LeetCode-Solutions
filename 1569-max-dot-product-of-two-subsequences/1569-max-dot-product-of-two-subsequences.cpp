ff8ed25fe4e7ab90fa512106b572767e0d4f1e92
pachauriyash <86353193+pachauriyash@users.noreply.github.com>
2023-10-26 13:03:28 +0530

    Time: 56 ms (19.38%), Space: 14 MB (9.38%) - LeetHub

--- 1569-max-dot-product-of-two-subsequences/1569-max-dot-product-of-two-subsequences.cpp
+++ 1569-max-dot-product-of-two-subsequences/1569-max-dot-product-of-two-subsequences.cpp
@@ -1,22 +1,18 @@
 class Solution {
 public:
     int recur(int i,int j,vector<int>&nums1,vector<int>&nums2,vector<vector<int>>&dp){
-        if(i==nums1.size()){
-            if(j==0)return INT_MIN;
-            return 0;
+        if(i==nums1.size() || j==nums2.size()){
+            return -1e9;
         }
-        //if(mp.find({i,j})!=mp.end())return mp[{i,j}];
         if(dp[i][j]!=INT_MIN)return dp[i][j];
-        int nottake=recur(i+1,j,nums1,nums2,dp);
-        int take=INT_MIN;
-        for(int k=j;k<nums2.size();k++){
-            take=max(take,nums1[i]*nums2[k]+recur(i+1,k+1,nums1,nums2,dp));
-        }
-        //mp.insert({{i,j},max(take,nottake)});
-        return dp[i][j]=max(take,nottake);
+        // int take=INT_MIN;
+        // for(int k=j;k<nums2.size();k++){
+        //     take=max(take,nums1[i]*nums2[k]+recur(i+1,k+1,nums1,nums2,dp));
+        // }
+        int take=max({nums1[i]*nums2[j],nums1[i]*nums2[j]+recur(i+1,j+1,nums1,nums2,dp),recur(i+1,j,nums1,nums2,dp),recur(i,j+1,nums1,nums2,dp)});
+        return dp[i][j]=take;
     }
     int maxDotProduct(vector<int>& nums1, vector<int>& nums2) {
-        map<pair<int,int>,int>mp;
         vector<vector<int>>dp(nums1.size()+1,vector<int>(nums2.size()+1,INT_MIN));
         return recur(0,0,nums1,nums2,dp);
     }
